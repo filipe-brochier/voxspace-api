e4ebc8b5ba7589df568c3a59f6402a64ef809e0e
Filipe Brochier <filipe@clae.com.br>
2025-04-08 10:09:37 -0300

    chore: add and set ts-config-paths

--- src/domain/entities/value-objects/slug.spec.ts
+++ src/domain/entities/value-objects/slug.spec.ts
@@ -1,8 +1,7 @@
-import { it, expect } from 'vitest'
 import { Slug } from './slug'
 
 it('should be able to create a slug from test', () => {
-  const slug = Slug.createFromString('Example Title')
+  const slug = Slug.createFromText('Example Title')
 
   expect(slug.value).toEqual('example-title')
 })
